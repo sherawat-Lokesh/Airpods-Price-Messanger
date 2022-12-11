74f6216c6fe06f4fc52823ec4b81aa760b277ace
sherawat-Lokesh <107927380+sherawat-Lokesh@users.noreply.github.com>
2022-12-11 16:42:38 +0530

    added flipkart scraping

--- Airpods.js
+++ Airpods.js
@@ -11,45 +11,78 @@ const app=express()
 dotenv.config()
 const accountSid=process.env.TWILIO_ACCOUNT_SID;
 const authToken=process.env.TWILIO_AUTH_TOKEN;
+const myMobileNum=process.env.MY_MOBILE_NUMBER;
 const client=new twilio(accountSid, authToken);
 
-const myMobileNum=process.env.MY_MOBILE_NUMBER;
-const url='https://www.amazon.in/Apple-AirPods-MagSafe-Charging-Case/dp/B09JR27TBV/ref=sr_1_5?keywords=airpods&qid=1670744415&sr=8-5'
+const amazon='https://www.amazon.in/Apple-AirPods-MagSafe-Charging-Case/dp/B09JR27TBV/ref=sr_1_5?keywords=airpods&qid=1670744415&sr=8-5'
+
+const flipkart='https://www.flipkart.com/apple-airpods-pro-magsafe-charging-case-bluetooth-headset/p/itm0847811966368?pid=ACCG7XG5ZQ47ZHFR&lid=LSTACCG7XG5ZQ47ZHFRGTXJL1&marketplace=FLIPKART&q=airpods+pro&store=0pm%2Ffcn&srno=s_1_1&otracker=search&otracker1=search&fm=Search&iid=be5647f3-3df1-4097-9b50-c33339aef983.ACCG7XG5ZQ47ZHFR.SEARCH&ppt=sp&ppn=sp&ssid=yyygnd24g00000001670755855354&qH=34bcf44f67ad7e7f'
+
+//! here interval is working is per-hour
 const hour=3600000
 const handle=setInterval(() => {
             SendMessage()
+            scrapeFlipkart()
         }, hour);
 
+
+
+// ! this function is for amazon scraping
 async function SendMessage(){
 
-    const response=await fetch(url)
+    const response=await fetch(amazon)
     const body=await response.text()
     const $=cheerio.load(body)
 
-        const obj={
-            productName:''
-            ,price:''
-            ,link:url
-        }
-            const name=$('#productTitle').text().trim().split(' ')
-            obj.productName=name[0]+' '+name[1]
+    const obj={productName:'',price:'',link:amazon}
+       
+            const name=$('#productTitle').text().trim().split(' ');
+            obj.productName=`${name[0]} ${name[1]} ${name[2]}`;
+
             obj.price= $('span.a-price.aok-align-center.reinventPricePriceToPayMargin.priceToPay > span.a-offscreen').text().replace(/[₹,]/g,'').split('.')[0]
 
         if(obj.price===''){SendMessage()
             return}
-        console.log('price',obj.price)
-            if(+obj.price < 16000){
-                client.messages
-                    .create({
-                        body: `Price for ${obj.productName} went down to ₹${+obj.price} buy it now \n ${obj.link}`,
+        console.log('Amazon-price:',+obj.price)
+
+        if(+obj.price < 16000){
+            sendSMS(obj.productName,obj.price,obj.link)
+            }
+
+}
+SendMessage();
+
+//! this function is for sending sms to mobile no.
+function sendSMS(productName,price,link){
+ client.messages.create({
+                        body: `\n🎉 Price for ${productName} went down to ₹${+price} buy it now \n ${link}`,
                         from: '+19704401504', // From a valid Twilio number
                         to: myMobileNum, // Text this number
                     })
                     .then((message) => console.log(message));
-                clearInterval(handle)
+                clearInterval(handle);
+}
+
+//! This function is for flipkart scraping
+async function scrapeFlipkart(){
+    const response= await fetch(flipkart);
+    const body=await response.text();
+    const $=cheerio.load(body);
+    const obj={productName:'',price:'',link:amazon}
+
+    obj.price=$('div.CEmiEU > div > div._30jeq3._16Jk6d').text().replace(/[₹,]/g, '');
+    const Name=$(' div:nth-child(1) > h1 > span').text().split(' ')
+        obj.productName=(`${Name[0]} ${Name[1]} ${Name[2]}`)
+
+         if(obj.price===''){scrapeFlipkart()
+            return}
+        console.log('Flipkart-price:',+obj.price)
+
+            if(+obj.price < 16000){
+               sendSMS(obj.productName,obj.price,obj.link)
             }
 
-}SendMessage()
+}scrapeFlipkart()
 
 
 

9935fee0f9afd4360d1179690a4163af4c098b65
SHERAWAT LOKESH <107927380+sherawat-Lokesh@users.noreply.github.com>
2022-12-12 08:58:07 +0530

    Update Airpods.js

--- Airpods.js
+++ Airpods.js
@@ -20,7 +20,7 @@ const flipkart='https://www.flipkart.com/apple-airpods-pro-magsafe-charging-case
 
 //! here interval is working is per-hour
 let totalCallInterval=0;
-const hour=3600000/2;
+const hour=900000;      //15 min
 const handle=setInterval(() => {
             SendMessage()
             scrapeFlipkart()
